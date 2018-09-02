308cf054d1b226f4c7f91ec04d413732195dc467
Vladislav_747 <zippo585@gmail.com>
2018-09-02 23:21:35 +0500

    Common Commmon common

--- app/src/main/java/com/example/melo/services_startandroid/MainActivity.java
+++ app/src/main/java/com/example/melo/services_startandroid/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnService_92, btnService_93;
+    Button btnService_92, btnService_93, btnService_94, getBtnService_95s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +16,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnService_92 = (Button) findViewById(R.id.btnService_92);
         btnService_93 =  (Button) findViewById(R.id.btnService_93);
+        btnService_94 =  (Button) findViewById(R.id.btnService_93);
 
         btnService_92.setOnClickListener(this);
         btnService_93.setOnClickListener(this);
+        btnService_94.setOnClickListener(this);
     }
 
 
@@ -31,7 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.btnService_93:
-                intent = new Intent(this, Service92.class);
+                intent = new Intent(this, Service93.class);
+                startActivity(intent);
+                break;
+            case R.id.btnService_94:
+                intent = new Intent(this, Service94.class);
                 startActivity(intent);
                 break;
         }
